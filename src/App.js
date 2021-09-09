5440e0f00cd42409458165770f87cdd5cfd4a8e0
Igor <igormgg@hotmail.com>
2021-09-09 11:49:14 -0300

    feat: added buttons code and styles

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import Characters from "./components/Characters";
 
 function App() {
   const [characterList, setCharacterList] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/")
+    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
       .then((response) => response.json())
-      // .then((response) => console.log(response));
       .then((response) => setCharacterList(response.results))
       .catch((err) => console.log(err));
-  }, []);
+  }, [page]);
+
+  const previousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
 
   return (
     <div className="App">
@@ -21,6 +31,10 @@ function App() {
       <body>
         <main>
           <Characters CharacterList={characterList} />
+          <div className="buttons">
+            <button onClick={previousPage}>Previous</button>
+            <button onClick={nextPage}>Next</button>
+          </div>
         </main>
       </body>
     </div>
